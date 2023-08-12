684b30d4112b0233316b666913b8008bb803c4ea
yamorigg <67684226+yamorigg@users.noreply.github.com>
2023-08-12 14:00:50 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='work-space2'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
